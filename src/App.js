98f4cb6e8dc072a8043805969d9de71ecbdc0a68
Anubha13kumari <er.chandraprakash1999@gmail.com>
2024-07-19 16:29:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Navbar from './Navbar.jsx';
+import Home from './Home.jsx';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,165 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import { RiHomeSmile2Fill } from "react-icons/ri";
+
+const pages = ["Rent", "Buy", "Sell"];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Container
+          style={{
+            backgroundColor: "White",
+          }}
+          maxWidth="auto"
+        >
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                color: "black",
+                textDecoration: "none",
+                alignItems: "center",
+              }}
+            >
+              <div className="nav__icon">
+                <RiHomeSmile2Fill />
+              </div>
+              Estatery
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="primary"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                color: "black",
+                textDecoration: "none",
+              }}
+            >
+              <div className="nav__icon">
+                <RiHomeSmile2Fill />
+              </div>
+              Estatery
+            </Typography>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+                alignItems: "center",
+              }}
+            >
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    my: 2,
+                    color: "#8c52ff",
+                    display: "block",
+                  }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Button
+                style={{
+                  backgroundColor: "white",
+                  color: "#8c52ff",
+                  marginRight: "1rem",
+                }}
+                variant="outlined"
+              >
+                Login
+              </Button>
+              <Button
+                style={{
+                  backgroundColor: "#8c52ff",
+                  color: "white",
+                }}
+                variant="contained"
+              >
+                Sign Up
+              </Button>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Navbar;
